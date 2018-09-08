01df0bba17702a91243db84e55b093bc92e8f978
Glitch (microscope-t1) <none>
2018-09-08 20:24:54 +0000

    🍔🍢 Checkpoint ./script.js:378534/1481

--- script.js
+++ script.js
@@ -11,17 +11,30 @@ function init() {
 }
 
 let r = 0, g = 0, b  = 0, a = 0.5
-
-
+let counter = 0
 
 function render() {
-  //ctx.beginPath()
+  ctx.beginPath()
+  counter++
   
   // center
   /* radius: 2 - 10px
    * r: 71-200, g: 50-170, b: 64-200, a: 0.4-0.9 
    */
-  
+  if (counter < 300) {
+    r = Math.floor(Math.random() * 200 + 71)
+    g = Math.floor(Math.random() * 170 + 50)
+    b = Math.floor(Math.random() * 200 + 64)
+    //a = Math.floor(Math.random() * 1.4)
+    ctx.strokeStyle = `rgba(${r},${g},${b},${a})`
+    
+    ctx.translate(Math.sin(counter + canvas.width), Math.cos(counter + canvas.height))
+    ctx.arc(Math.sin(counter + canvas.width), 
+            Math.cos(counter + canvas.height), Math.floor(Math.random() * 10 - 2), 0, 2 * Math.PI)
+    ctx.stroke()
+    ctx.closePath()
+    ctx.translate(-canvas.width, -canvas.height)
+  }
   
   // middle
   
@@ -29,10 +42,7 @@ function render() {
   
   
   // draw
-  console.log(ctx)
-  ctx.strokeStyle(`rgba({r},${g},${b},${a})`)
-  ctx.arc(100, 75, 50, 0, 2 * Math.PI)
-  ctx.stroke()
+  
   //requestAnimationFrame(render)
 }
   
