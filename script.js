16d8d5ab884ccdf77b3b7768957646666b37dcdd
Glitch (microscope-t1) <none>
2018-09-08 20:57:28 +0000

    🏓⏱ Checkpoint ./script.js:378534/1242

--- script.js
+++ script.js
@@ -7,50 +7,49 @@ canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 
 function init() {
-  ctx.translate(canvas.width / 3, canvas.height / 3)
+  ctx.translate(canvas.width / 2, canvas.height / 2)
 }
 
 let r = 0, g = 0, b  = 0, a = 0.5
 let counter = 0
 
 function render() {
-  
-  counter++
-  
   // center
   /* radius: 2 - 10px
    * r: 71-200, g: 50-170, b: 64-200, a: 0.4-0.9 
    */
-  if (counter < 600) {
+  if (counter < 100) {
+    counter += 0.8
     r = Math.floor(Math.random() * 200 + 101)
     g = Math.floor(Math.random() * 80 + 50)
     b = Math.floor(Math.random() * 190 + 64)
-    //a = Math.floor(Math.random() * 1.4)
+    a = Math.random() * 0.4
     ctx.beginPath()
-    ctx.strokeStyle = `rgba(${r},${g},${b},${a})`
+    ctx.fillStyle = `rgba(${r},${g},${b},${a})`
+    ctx.strokeStyle = 'rgba(255, 255, 255, 0.3)'
     //ctx.transform(canvas.width, canvas.height)
-    ctx.translate(Math.sin(30), Math.cos(30))
-    ctx.arc(Math.sin(counter) / 2, 
-            Math.cos(counter) / 2,
-            10, 0, 2 * Math.PI)
+    ctx.translate(Math.cos(counter) * counter, Math.sin(counter) * counter)
+    ctx.arc(Math.sin(counter), 
+            Math.cos(counter),
+            counter, 0, 2 * Math.PI)
     ctx.stroke()
+    ctx.fill()
     ctx.closePath()
     //ctx.transform(-canvas.width, -canvas.height)
-  }
-  
-  // middle
-  if (counter > 600) {
-    r = Math.floor(Math.random() * 200 + 71)
-    g = Math.floor(Math.random() * 110 + 50)
-    b = Math.floor(Math.random() * 200 + 64)
-    //a = Math.floor(Math.random() * 1.4)
+  } else {
+    counter += 1
+    r = Math.floor(Math.random() * 20 + 11)
+    g = Math.floor(Math.random() * 210 + 100)
+    b = Math.floor(Math.random() * 210 + 104)
+    a = Math.random() * 0.99 - 0.4
     ctx.beginPath()
-    ctx.strokeStyle = `rgba(${r},${g},${b},${a})`
-    //ctx.transform(canvas.width, canvas.height)
-    ctx.translate(Math.sin(Math.log(counter) * canvas.width) * 0.632, Math.cos(Math.log(counter) + canvas.height) * 10.861)
-    ctx.arc(Math.sin(Math.log(counter) + (canvas.width / 2)), 
-            Math.sin(Math.log(counter) + (canvas.height / 2)), Math.floor(Math.random() * 120 - 2), 0, 2 * Math.PI)
-    ctx.stroke()
+    ctx.fillStyle = `rgba(${r},${g},${b},${a})`
+    ctx.strokeStyle = `rgba(255, 255, 255, ${a})`
+    ctx.translate(Math.cos(counter) * counter, Math.sin(counter) * counter)
+    ctx.arc(Math.sin(counter), 
+            Math.cos(counter),
+            counter * 1.5, 0, 2 * Math.PI)
+    ctx.fill()
     ctx.closePath()
     //ctx.transform(-canvas.width, -canvas.height)
   }  
