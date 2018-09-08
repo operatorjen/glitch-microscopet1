f54eb7a608d12bafb58324fa4a65a0ebeede0049
Glitch (microscope-t1) <none>
2018-09-08 21:18:33 +0000

    ☔️🌆 Checkpoint ./script.js:378534/1880

--- script.js
+++ script.js
@@ -18,40 +18,48 @@ function render() {
   /* radius: 2 - 10px
    * r: 71-200, g: 50-170, b: 64-200, a: 0.4-0.9 
    */
-  if (counter < 250) {
-    counter += 0.9
-    r = Math.floor(Math.random() * 120 + 11)
-    g = Math.floor(Math.random() * 210 + 100)
-    b = Math.floor(Math.random() * 210 + 104)
-    a = Math.random() * 0.8
-    ctx.beginPath()
-   // ctx.fillStyle = `rgba(${r},${g},${b},${a})`
-    ctx.strokeStyle = `rgba(${r},${g},${b},${a})`
-    ctx.strokeWidth = 1
-    ctx.translate(Math.cos(counter) * counter, Math.sin(counter) * counter)
-    ctx.arc(Math.sin(counter) / 6, 
-            Math.cos(counter) / 3,
-            Math.random() * 10 - 2, 0, 2 * Math.PI)
-   // ctx.fill()
-    ctx.stroke()
-    ctx.closePath()
-  } else {
-    counter += 0.99
+  if (counter < 90) {
+    counter += 0.60334834
     r = Math.floor(Math.random() * 200 + 101)
     g = Math.floor(Math.random() * 80 + 50)
     b = Math.floor(Math.random() * 190 + 64)
-    a = Math.random() * 0.4
+    a = Math.random() * 0.8
+    
+    if (counter > 70) {
+      r = Math.floor(Math.random() * 220 + 151)
+      g = Math.floor(Math.random() * 40 + 10)
+      b = Math.floor(Math.random() * 150 + 64)
+      a = Math.random() * 0.99 - 0.6  
+    }
+    
     ctx.beginPath()
-    ctx.strokeWidth = 1
-  //  ctx.fillStyle = `rgba(${r},${g},${b},${a})`
+    ctx.fillStyle = `rgba(${r},${g},${b},${a})`
+    ctx.strokeStyle = `rgba(${r},${g},${b},${a})`
+    ctx.strokeWidth = 3
+    ctx.translate(Math.cos(counter) * counter, Math.sin(counter) * counter)
+    ctx.arc(Math.sin(counter / 3), 
+            Math.cos(counter / 3),
+            counter / 6, 0, 2 * Math.PI)
+    ctx.fill()
+    ctx.stroke()
+    ctx.closePath()
+  } else {
+    counter += 1.29
+    r = Math.floor(Math.random() * 120 + 11)
+    g = Math.floor(Math.random() * 210 + 100)
+    b = Math.floor(Math.random() * 210 + 104)
+    a = Math.random() * 0.99 - 0.4
+    ctx.beginPath()
+    ctx.strokeWidth = 4
+    ctx.fillStyle = `rgba(${r},${g},${b},${a})`
     ctx.strokeStyle = `rgba(${r},${g},${b},${a})`
     //ctx.transform(canvas.width, canvas.height)
-    ctx.translate(Math.cos(counter) * counter, Math.sin(counter) * counter)
-    ctx.arc(Math.sin(counter), 
-            Math.cos(counter),
+    ctx.translate(Math.sin(counter) * counter, Math.cos(counter) * counter)
+    ctx.arc(Math.sin(counter / 2), 
+            Math.cos(counter / 4),
             counter / 5, 0, 2 * Math.PI)
     ctx.stroke()
-    //ctx.fill()
+    ctx.fill()
     ctx.closePath()
     //ctx.transform(-canvas.width, -canvas.height)
   }  
