f93dafdb39819726a5da66339c5935b42a258e24
Glitch (microscope-t1) <none>
2018-09-08 20:04:00 +0000

    🍓🌼 Checkpoint ./script.js:378534/1071 ./index.html:378534/557 ./style.css:378534/134

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+// sample from https://kr.123rf.com/photo_74959276_%ED%98%84%EB%AF%B8%EA%B2%BD%EB%B3%B4%EA%B8%B0%EC%97%90%EC%84%9C-%EC%8B%9D%EB%AC%BC-%EB%BF%8C%EB%A6%AC%EC%9D%98-%EB%8B%A8%EB%A9%B4.html
 
-// prints "hi" in the browser's dev tools console
-console.log('hi');
+const canvas = document.querySelector('canvas')
+const ctx = canvas.getContext('2d')
+
+canvas.width = window.innerWidth 
+canvas.height = window.innerHeight
+
+function init() {
+  ctx.translate(canvas.width / 3, canvas.height / 3)
+}
+
+function render() {
+  //ctx.beginPath()
+  ctx.arc(100, 75, 50, 0, 2 * Math.PI)
+  ctx.stroke()
+  requestAnimationFrame(render)
+}
+  
+window.onresize = function () {
+  canvas.width = window.innerWidth
+  canvas.height = window.innerHeight
+  init()
+}
+
+init()
+render()
