f867346e63d6b5c1807859258c7e68fe535600a3
Glitch (microscope-t1) <none>
2018-09-08 20:46:26 +0000

    📹🚁 Checkpoint ./script.js:378534/1437

--- script.js
+++ script.js
@@ -22,21 +22,39 @@ function render() {
    * r: 71-200, g: 50-170, b: 64-200, a: 0.4-0.9 
    */
   if (counter < 600) {
-    r = Math.floor(Math.random() * 200 + 71)
-    g = Math.floor(Math.random() * 170 + 50)
-    b = Math.floor(Math.random() * 200 + 64)
+    r = Math.floor(Math.random() * 200 + 101)
+    g = Math.floor(Math.random() * 80 + 50)
+    b = Math.floor(Math.random() * 190 + 64)
     //a = Math.floor(Math.random() * 1.4)
+    ctx.beginPath()
     ctx.strokeStyle = `rgba(${r},${g},${b},${a})`
     //ctx.transform(canvas.width, canvas.height)
-    ctx.translate(Math.sin(Math.log(counter) * canvas.width) * 0.632, Math.cos(Math.log(counter) + canvas.height) * 0.861)
-    ctx.arc(Math.sin(counter + canvas.width) * counter, 
-            Math.cos(counter + canvas.height), Math.floor(Math.random() * 20 - 2), 0, 2 * Math.PI)
+    ctx.translate(Math.sin(30), Math.cos(30))
+    ctx.arc(Math.sin(counter) / 2, 
+            Math.cos(counter) / 2,
+            10, 0, 2 * Math.PI)
     ctx.stroke()
-    //ctx.closePath()
+    ctx.closePath()
     //ctx.transform(-canvas.width, -canvas.height)
   }
   
   // middle
+  if (counter > 600) {
+    r = Math.floor(Math.random() * 200 + 71)
+    g = Math.floor(Math.random() * 110 + 50)
+    b = Math.floor(Math.random() * 200 + 64)
+    //a = Math.floor(Math.random() * 1.4)
+    ctx.beginPath()
+    ctx.strokeStyle = `rgba(${r},${g},${b},${a})`
+    //ctx.transform(canvas.width, canvas.height)
+    ctx.translate(Math.sin(Math.log(counter) * canvas.width) * 0.632, Math.cos(Math.log(counter) + canvas.height) * 10.861)
+    ctx.arc(Math.sin(Math.log(counter) + (canvas.width / 2)), 
+            Math.sin(Math.log(counter) + (canvas.height / 2)), Math.floor(Math.random() * 120 - 2), 0, 2 * Math.PI)
+    ctx.stroke()
+    ctx.closePath()
+    //ctx.transform(-canvas.width, -canvas.height)
+  }  
+
   
   // outer
   
